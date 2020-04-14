377b3e198533658c22cb1ae3df8736023c82fbd7
zhanfan9812 <zhanfan9812@users.noreply.github.com>
2020-04-14 18:51:22 +0800

    界面404问题

---
+++ example/test.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:QQ19981212@127.0.0.1/python'
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password= db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return "<Role %r>"%self.name
--- sqlTest.py
+++
@@ -1,9 +0,0 @@
-from test import db
-from test import User
-db.create_all()
-admin = User(username='t', email='t@example.com')
-guest = User(username='bttbb', email='bttbb@example.com')
-db.session.add(admin)
-db.session.add(guest)
-db.session.commit()
-print(User.query.all())

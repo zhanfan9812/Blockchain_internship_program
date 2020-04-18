be3c3925abfafa6ee241545d9b0b47789be1422f
zhanfan9812 <zhanfan9812@users.noreply.github.com>
2020-04-18 17:09:53 +0800

    finish logistician

--- static/js/commmodities_list.js
+++ static/js/commmodities_list.js
@@ -5,6 +5,7 @@ $(function () {
    $.ajax({
        url:"/getArriveCommList/"+status,
        success:function (data) {
+//          alert(status)
           var html = "";
           for (var i = 0;i<data.data.length;i++){
               var j = i+1;
@@ -17,8 +18,8 @@ $(function () {
                   "\t\t\t\t\t\t\t\t\t<td>"+data.data[i].date_of_pro+"</td>\n" +
                   "\t\t\t\t\t\t\t\t\t<td>"+data.data[i].description+"</td>\n" +
                   "\t\t\t\t\t\t\t\t\t<td>\n" +
-                  "\t\t\t\t\t\t\t\t\t\t<a class=\"layui-btn layui-btn-sm layui-btn-normal\" title=\"编辑\" onclick=\"execute_open('更新商品状态', 'warehouseUpdateStatus.html?id="+data.data[i].id+"', 1000, 300)\" href=\"javascript:;\"><i class=\"layui-icon layui-icon-edit\"></i>编辑</a>\n" +
-                  "\t\t\t\t\t\t\t\t\t\t<a class=\"layui-btn layui-btn-sm layui-btn-normal\" title=\"编辑\" onclick=\"execute_open('更新商品状态', 'warehouseUpdateStatus.html?id="+data.data[i].id+"', 1000, 300)\" href=\"javascript:;\"><i class=\"layui-icon layui-icon-edit\"></i>物流二维码</a>\n" +
+                  "\t\t\t\t\t\t\t\t\t\t<a class=\"layui-btn layui-btn-sm layui-btn-normal\" title=\"编辑\" onclick=\"execute_open('更新商品状态', 'warehouseUpdateStatus"+status+".html?id="+data.data[i].id+"', 1000, 300)\" href=\"javascript:;\"><i class=\"layui-icon layui-icon-edit\"></i>编辑</a>\n" +
+                  "\t\t\t\t\t\t\t\t\t\t<a class=\"layui-btn layui-btn-sm layui-btn-normal\" title=\"编辑\" onclick=\"execute_open('更新商品状态', 'warehouseUpdateStatus"+status+".html?id="+data.data[i].id+"', 1000, 300)\" href=\"javascript:;\"><i class=\"layui-icon layui-icon-edit\"></i>物流二维码</a>\n" +
                   "\t\t\t\t\t\t\t\t\t</td>\n" +
                   "\t\t\t\t\t\t\t\t</tr>"
           }
